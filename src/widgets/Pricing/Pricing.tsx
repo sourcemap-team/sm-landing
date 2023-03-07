50a712f72cdc0fe26a53fa43411643b981451581
RostislavBartenev <rostislav.bartenev@gmail.com>
2023-03-07 23:20:13 +0300

    mobile

--- src/widgets/Pricing/Pricing.tsx
+++ src/widgets/Pricing/Pricing.tsx
@@ -9,21 +9,29 @@ export const Pricing = () => {
       <h2 className={cx('title', styles.title)}>Pricing</h2>
       <div className={styles.info}>
         <div className={styles.block}>
-          <p className={cx(styles.light, styles.textShort)}>
+          <p className={cx(styles.light, styles.textShort, styles.text)}>
             We work according to Time & Material, so the client pays for the
             actual time spent by our specialists on the project.
           </p>
-          <span className={cx(styles.minPrice, styles.light)}>$50/hour</span>
-          <p className={cx(styles.time, styles.light)}>
-            Up to 100 hours per month
-          </p>
+          <div className={styles.basicWidth}>
+            <span className={cx(styles.minPrice, styles.light)}>$50/hour</span>
+            <p className={cx(styles.time, styles.light)}>
+              Up to 100 hours per month
+            </p>
+          </div>
         </div>
         <div className={styles.block}>
-          <p className={cx(styles.light, styles.textLong)}>
+          <p className={cx(styles.light, styles.textLong, styles.text)}>
             We work in sprints lasting 1-2 weeks, adapt flexibly to the
             client&apos;s processes, provide transparent reports on the work
             done, and always guarantee the result.
           </p>
+          <div className={styles.tabletWidth}>
+            <span className={cx(styles.minPrice, styles.light)}>$50/hour</span>
+            <p className={cx(styles.time, styles.light)}>
+              Up to 100 hours per month
+            </p>
+          </div>
           <span className={styles.price}>$45/hour</span>
           <p className={cx(styles.time, styles.light)}>
             Over 100 hours per month
