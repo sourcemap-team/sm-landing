8f00adb692ff41a639fd4cd33a7a7bc89751adb2
Roman Babanov <babanov.js@gmail.com>
2023-03-25 15:12:31 +0300

    add ya metrika and twitter meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,45 @@ const Home = () => {
           content={META.description[lang]}
           key="description"
         />
-        <meta property="og:image" content="/images/share/sourcemap-social-share.jpg" />
+        <meta property="og:image" itemProp="image" content="/images/share/sourcemap-social-share.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={META.title[lang]} />
+        <meta name="twitter:site" content="@sourcemap.pro" />
+        <meta
+          name="twitter:image"
+          content="/images/share/sourcemap-social-share.jpg"
+        />
+        <meta name="twitter:image:alt" content={META.title[lang]} />
+
+        <script
+          id="yandex-metrika"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            
+            ym(88956706, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });`,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/88956706"
+                style={{ position: 'absolute', left: '-9999px' }}
+                alt="yandex-metrika-noscript"
+              />
+            </div>`,
+          }}
+        />
       </Head>
       <main className={styles.main}>
         <Hero />
